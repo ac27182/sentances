86298f5b3bee0e4c551b635c1fd420982260e7fe
Alex Cameron <alex.cameron@treasure-data.com>
2024-03-25 16:01:05 +0900

    ..

--- modules/compiler/anki_compile.ts
+++ modules/compiler/anki_compile.ts
@@ -1,5 +1,18 @@
 import { readFileSync, writeFileSync } from "fs"
 
+const makeCardTemplate = (input: { kanji: string, hiragana: string, translation: string }) => {
+
+  const _word = `<a href="https://jisho.org/search/${input.kanji}">言葉</a>`
+
+  const kanji = `<a href="https://jisho.org/search/${input.kanji}%23kanji">漢字</a>`
+
+  const picture = `<a href="https://www.google.com/search?tbm=isch&q=${input.kanji}">写真</a>`
+
+  const template = `<span>${input.hiragana}・${input.translation}</span><br>${kanji}・${_word}・${picture}`
+
+  return [input.kanji, template]
+}
+
 export const anki_compile = (name: string) =>
   writeFileSync(
     `data/csv_anki/${name}.csv`,
@@ -7,12 +20,7 @@ export const anki_compile = (name: string) =>
       .split("\n")
       .map(row => row.split(","))
       .map(([kanji, hiragana, translation]) => { return { kanji, hiragana, translation } })
-      .map(row => [
-        row.kanji,
-        (row.translation === undefined)
-          ? row.hiragana
-          : `${row.hiragana}・${row.translation}`
-      ])
+      .map(makeCardTemplate)
       .map(row => row.join(","))
       .join("\n")
   )
