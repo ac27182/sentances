f5eb23a519c93ce16e83b73ed3a3ade9099098c4
Alex Cameron <alex.cameron@treasure-data.com>
2025-12-19 10:44:33 +0900

    updating

--- modules/jlpt_vocab/index.ts
+++ modules/jlpt_vocab/index.ts
@@ -2,6 +2,10 @@ import { randomUUID } from "node:crypto"
 import fs from "node:fs"
 
 const n5_vocab = fs.readFileSync('modules/jlpt_vocab/n5.tsv', 'utf-8')
+const n4_vocab = fs.readFileSync('modules/jlpt_vocab/n4.tsv', 'utf-8')
+// const n3_vocab = fs.readFileSync('modules/jlpt_vocab/n3.tsv', 'utf-8')
+// const n2_vocab = fs.readFileSync('modules/jlpt_vocab/n2.tsv', 'utf-8')
+// const n1_vocab = fs.readFileSync('modules/jlpt_vocab/n1.tsv', 'utf-8')
 
 const SEPARATOR = '|'
 
@@ -23,15 +27,22 @@ const rows = [
     ...headers
 ]
 
-n5_vocab
-    .split('\n')
+const vocab = [
+    ...n5_vocab.split('\n'),
+    ...n4_vocab.split('\n'),
+    // ...n3_vocab.split('\n'),
+    // ...n2_vocab.split('\n'),
+    // ...n1_vocab.split('\n'),
+]
+
+vocab
     .map(row => row.split('\t'))
     .map(row => {
-        const [kanji, hiragana, meaning, tag, guid] = row
+        const [kanji, hiragana, english, tag, guid] = row
 
-        const front = `${kanji}</br>${hiragana}`
+        const front = `<div class="kanji">${kanji}</div><div class="hiragana">${hiragana}</div>`
 
-        const back = meaning
+        const back = `<div class="english">${english}</div>`
 
         const GUID = guid.substring(0, 32)
 
